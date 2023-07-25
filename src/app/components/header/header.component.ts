9ea97baa802fc395d1fc34afa8e60205328573bc
João Pedro <jp663687@gmail.com>
2023-07-25 09:47:13 -0300

    adiciona `menu`

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,43 @@
 import { Component } from '@angular/core';
+import { PoMenuPanelItem } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  title: string = 'Customers';
+
+  public readonly menuItems: Array<PoMenuPanelItem> = [
+    {
+      label: 'Home',
+      action: this.changeTitle.bind(this),
+      icon: 'po-icon-home',
+    },
+    {
+      label: 'Customers',
+      action: this.changeTitle.bind(this),
+      icon: 'po-icon-user',
+    },
+    {
+      label: 'New Sale',
+      action: this.changeTitle.bind(this),
+      icon: 'po-icon-money',
+    },
+    {
+      label: 'Reports',
+      action: this.changeTitle.bind(this),
+      icon: 'po-icon-news',
+    },
+    {
+      label: 'Settings',
+      action: this.changeTitle.bind(this),
+      icon: 'po-icon-settings',
+    },
+  ];
+
+  changeTitle(menu: PoMenuPanelItem) {
+    this.title = menu.label;
+  }
+}
